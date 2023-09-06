068f91821b086646b37aa2745cd9895a2b4efcbd
Nihar Hegde <niharhegde163@gmail.com>
2023-09-06 21:55:05 +0530

    peoples page done

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,4 +1,6 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 
 export default async function UserLayout({
@@ -6,9 +8,12 @@ export default async function UserLayout({
 }:{
     children:React.ReactNode;
 }){
+    const users = await getUsers();
     return(
         <Sidebar>
-            <div className="h-full">{children}</div>
+            <div className="h-full">
+                <UserList items = {users}/>
+                {children}</div>
         </Sidebar>
     )
 }
